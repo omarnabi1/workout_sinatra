7b293f06278b640e9f769371ed80045db0e4009a
Omar Nabi <onabi20@gmail.com>
2020-05-05 02:52:47 +0000

    add descp

---
+++ lakers_cli/lib/lakers/cli.rb
@@ -0,0 +1,8 @@
+require "lakers/cli/version"
+
+module Lakers
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lakers_cli/lib/lakers/cli/version.rb
@@ -0,0 +1,5 @@
+module Lakers
+  module Cli
+    VERSION = "0.1.0"
+  end
+end

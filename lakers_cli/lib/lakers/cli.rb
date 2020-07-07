9dc49896e0064c1fcd11fc4f1b44b38baecc4e39
Omar Nabi <onabi20@gmail.com>
2020-07-07 01:33:18 +0000

    second

--- lakers_cli/lib/lakers/cli.rb
+++
@@ -1,8 +0,0 @@
-require "lakers/cli/version"
-
-module Lakers
-  module Cli
-    class Error < StandardError; end
-    # Your code goes here...
-  end
-end
--- lakers_cli/lib/lakers/cli/version.rb
+++
@@ -1,5 +0,0 @@
-module Lakers
-  module Cli
-    VERSION = "0.1.0"
-  end
-end

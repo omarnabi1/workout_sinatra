cc3d70cec413985b9faeb13e788b8f309031782b
omarnabi1 <onabi20@gmail.com>
2020-07-07 21:22:36 -0400

    2.0

--- final-sinatra/app/controllers/users_controller.rb
+++ final-sinatra/app/controllers/users_controller.rb
@@ -1,65 +1,65 @@
 class UsersController < ApplicationController
 
-   
-   
+
+  
     post '/login' do
-        user = User.find_by(:username => params [:username])
-        if user && user.authenticate(params[:password])
-            session[:user_id] = user.id 
-            redirect to "/users/#{user.id}"
-        else
-            redirect to '/'
-        end
+      user = User.find_by(:username => params[:username])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id 
+        redirect to "/users/#{user.id}"
+      else
+        redirect to '/'
+      end
     
     end
-
-    post '/signup' do
-        if params[:username].empty? || params[:password].empty?
-            redirect to '/'
-        elsif user = User.find_by(:username => params[:username])
-            redirect to '/'
-        else 
-            user = User.create(params)
-            session[:user_id] = user.id
-            redirect to "/users/#{user.id}"
+  
+    post '/signup' do 
+      if params[:username].empty? || params[:password].empty?
+        redirect to '/'
+      elsif user = User.find_by(:username => params[:username])
+        redirect to '/'
+      else
+      user = User.create(params)
+      session[:user_id] = user.id
+      redirect to "/users/#{user.id}"
         end
     end
-
-    get '/logout' do
-        if session[:user_id] != nil
-            session.clear
-            redirect to '/'
-        end
+  
+    get '/logout' do 
+      if session[:user_id] != nil 
+        session.clear
+      redirect to '/'
+      end
     end
-
+    
     post "/users" do
-        @user = User.find_by(params[:user_id])
-        @user.save
-        if @user
-            session[:user_id] = @user.id
-        redirect "/users/#{user.id}"
-        end
+      @user = User.find_by(params[:user_id])
+      @user.save
+      if @user 
+        session[:user_id] = @user.id
+      redirect "/users/#{@user.id}"
+      end
     end
-
-
+  
+    
     get "/users/:id" do
-        if logged_in?
-            @user = User.find_by(:id => params[:id])
-            erb :"/users/show.html"
-        else
-            redirect to '/'
-        end
+      if logged_in?
+      @user = User.find_by(:id => params[:id])
+      erb :"/users/show"
+      else  
+        redirect to '/'
+      end
     end
-
+  
     get "/users/:id/edit" do 
-        if logged_in?
-            @user = User.find_by(:id => params[:id])
-            erb :"/users/edit.html"
-        else
-            redirect to '/'
-        end
+      if logged_in?
+        @user = User.find_by(:id => params[:id])
+        erb :"/users/edit"
+      else
+        redirect to '/'
+      end
     end
-
+  
     patch "/users/:id" do 
         @user = User.find_by(:id => params[:id])
         if  logged_in? && @user == current_user
@@ -85,10 +85,3 @@ class UsersController < ApplicationController
   
   
   end
-  
-
-
-
-
-
-
--- final-sinatra/app/controllers/workouts_controller.rb
+++ final-sinatra/app/controllers/workouts_controller.rb
@@ -1,24 +1,78 @@
 class WorkoutsController < ApplicationController
 
-
-    get "/workouts" do 
-        if logged_in? 
-            @workouts = Workout.all 
-            erb :"/workouts/index.html"
-        else
-            redirect to '/'
-        end
+ 
+    get "/workouts" do
+      if logged_in?
+      @workouts = Workout.all
+      erb :"/workouts/index"
+      else  
+        redirect to '/'
+      end
     end
-
-
-    get "/workouts/new" do 
+  
+  
+    get "/workouts/new" do
+      if logged_in?
+      erb :"/workouts/new"
+      else  
+        redirect to '/'
+      end
+    end
+  
+  
+    post "/workouts" do
+      if params[:name].empty? || params[:duration].empty? || params[:calories_burned].empty?
+        redirect to '/workouts/new'
+      end 
         if logged_in?
-            erb :"/workouts/new.html"
-        else
-            redirect to '/'
-        end
+          workout = Workout.create(params)
+          workout.user_id = current_user.id
+          workout.save
+          redirect "/workouts/#{workout.id}"
+      end
     end
-
-
-    post "/workouts" do 
-        if params[:name].empty? || params[:muscle_]
+  
+  
+    get "/workouts/:id" do
+      if @workout = Workout.find_by(:id => params[:id])
+      erb :"/workouts/show"
+      else  
+        redirect to '/'
+      end
+      
+    end
+  
+  
+    get "/workouts/:id/edit" do
+      if logged_in?
+      @workout = Workout.find_by(:id => params[:id])
+      erb :"/workouts/edit"
+      else
+     redirect to '/'
+      end
+    end
+  
+  
+    patch "/workouts/:id" do
+      @workout = Workout.find(params[:id])
+      if logged_in? && @workout.user_id == current_user.id 
+         @workout.update(params[:workout])
+         redirect "/workouts/#{@workout.id}"
+        else
+        redirect "/workouts/new"
+      end
+    end
+  
+    
+    delete "/workouts/:id" do
+      if logged_in?
+        @workout = Workout.find_by(:id => params[:id]) 
+        if @workout.user == current_user
+        @workout.destroy
+      redirect to "/users/#{current_user.id}"
+      end
+      else
+        redirect to '/'
+      end
+    end
+  end

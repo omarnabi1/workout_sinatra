b42563257ba87186cc7dc892d2622e25bd7fbbbc
omarnabi1 <onabi20@gmail.com>
2020-07-08 00:24:47 -0400

    3.0

--- final-sinatra/app/models/workout.rb
+++ final-sinatra/app/models/workout.rb
@@ -1,4 +1,4 @@
 class Workout < ActiveRecord::Base
     belongs_to :user
-    validates :name, :muscle_group, :duration, :calories_burned, presence: true
+    validates :name, :duration, :calories_burned, presence: true
 end

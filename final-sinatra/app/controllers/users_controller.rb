91a6df5b862778dc573c6ef1be5c0468faba96a1
omarnabi1 <onabi20@gmail.com>
2020-07-15 14:31:08 -0400

    why cant you just work

--- final-sinatra/app/controllers/users_controller.rb
+++ final-sinatra/app/controllers/users_controller.rb
@@ -1,3 +1,4 @@
+require './config/environment'
 class UsersController < ApplicationController
 
 
--- final-sinatra/app/controllers/workouts_controller.rb
+++ final-sinatra/app/controllers/workouts_controller.rb
@@ -1,3 +1,4 @@
+require './config/environment'
 class WorkoutsController < ApplicationController
 
  
@@ -39,7 +40,35 @@ class WorkoutsController < ApplicationController
         end
       end
     end
-        
+       
+    get "/workouts/:id" do
+      if logged_in?
+        @workout = Workout.find_by(:id => params[:id])
+        erb :"/workouts/show"
+      else
+        redirect to '/'
+      end
+    end
+  
+    get "/workouts/:id/edit" do
+      if logged_in?
+      workout = Workout.find_by(:id => params[:id])
+      erb :"/workouts/edit"
+      else
+     redirect to redirect to "/workouts/#{workout.id}"
+      end
+    end
+  
+    patch "/workouts/:id" do
+      workout = Workout.find(params[:id])
+      if logged_in? && workout.user_id == current_user.id 
+         workout.update(params[:workout])
+         redirect "/workouts/#{workout.id}"
+       
+        else
+        redirect "/workouts/new"
+      end
+    end
  
   
     

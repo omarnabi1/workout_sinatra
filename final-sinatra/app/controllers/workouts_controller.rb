48e225941fb87cbb08ee6e162a749fb29522b3de
omarnabi1 <onabi20@gmail.com>
2020-07-17 14:32:57 -0400

    The One They Call A

--- final-sinatra/app/controllers/workouts_controller.rb
+++ final-sinatra/app/controllers/workouts_controller.rb
@@ -1,7 +1,7 @@
 require './config/environment'
 class WorkoutsController < ApplicationController
 
- 
+  
     get "/workouts" do
       if logged_in?
       workouts = Workout.all
@@ -11,8 +11,7 @@ class WorkoutsController < ApplicationController
       end
     end
 
-  
-  
+
     get "/workouts/new" do
       if logged_in?
       erb :"/workouts/new"
@@ -21,8 +20,7 @@ class WorkoutsController < ApplicationController
     end
   end
 
-  
-  
+
   get "/workouts/:id" do
     if logged_in?
       @workout = Workout.find_by(:id => params[:id])
@@ -32,21 +30,22 @@ class WorkoutsController < ApplicationController
     end
   end  
 
+
   get "/workouts/:id/edit" do
     if logged_in?
-    workout = Workout.find_by(:id => params[:id])
+    @workout = Workout.find_by(:id => params[:id])
     erb :"/workouts/edit"
     else
-   redirect to redirect to "/workouts/#{workout.id}"
+   redirect to "/workouts/#{@workout.id}"
     end
   end
-  
+
+
   patch "/workouts/:id" do
     workout = Workout.find(params[:id])
     if logged_in? && workout.user_id == current_user.id 
-       workout.update(params[:workout])
+      workout.update(name: params[:name],duration: params[:duration],notes: params[:notes], )
        redirect "/workouts/#{workout.id}"
-     
       else
       redirect "/workouts/new"
     end
@@ -62,16 +61,13 @@ class WorkoutsController < ApplicationController
           puts workout.errors
           redirect to '/workouts/new' 
         end
-      
       else
         redirect to '/'
-   
-        
-      
         end
       end
     end
-       
+
+
   delete "/workouts/:id" do
       if logged_in?
         workout = Workout.find_by(:id => params[:id]) 
@@ -80,7 +76,6 @@ class WorkoutsController < ApplicationController
         redirect to "/users/#{current_user.id}"
       else
         redirect to '/'
-      
     end
   end
 end
